e037755f1c4337c14c00a89b62b55c2f5876616f
Milan Curcic <caomaco@gmail.com>
2017-08-13 14:25:51 -0400

    add base cloudrun library skeleton

---
+++ cloudrun/run.py
@@ -0,0 +1,10 @@
+"""
+run.py
+"""
+
+class Run(object):
+    """Class to handle all model run metadata 
+    and provide access to model output."""
+    def __init__(self):
+        """Run constructor."""
+        pass
