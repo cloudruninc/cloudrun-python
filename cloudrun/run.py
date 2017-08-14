101633a456de6689a89e8359a7ef63f577d26947
Milan Curcic <caomaco@gmail.com>
2017-08-14 18:44:37 -0400

    add Run methods skeleton

--- cloudrun/run.py
+++ cloudrun/run.py
@@ -8,3 +8,27 @@ class Run(object):
     def __init__(self,id):
         """Run constructor."""
         self.id = id
+
+    def create(self,model,version):
+        """Create a model run."""
+        pass
+
+    def delete(self):
+        """Deletes run data on server."""
+        pass
+
+    def get(self):
+        """Refreshes the run data from the server."""
+        pass
+
+    def upload(self,filename=None,url=None):
+        """Upload a local or remote file."""
+        pass
+
+    def start(self):
+        """Starts the run."""
+        pass
+
+    def start(self):
+        """Stops the run."""
+        pass
