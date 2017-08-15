bd7d8acabf2806fdb9aaeed2ed108b95fee6495c
Milan Curcic <caomaco@gmail.com>
2017-08-15 17:27:47 -0400

    fix name in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='cloudrun-python',
+    name='cloudrun',
     version='0.1.0',
     description='Python interface to Cloudrun API',
     author='Cloudrun Inc.',
