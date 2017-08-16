9e63a589dcb3b1c3596f4b9560e36c8d715e8ea1
Milan Curcic <caomaco@gmail.com>
2017-08-16 18:38:50 -0400

    enable API response mocking

--- cloudrun/tests.py
+++ cloudrun/tests.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+import responses
 import uuid
 from .cloudrun import Cloudrun
 from .run import Run
@@ -9,9 +12,27 @@ def test_cloudrun_init():
     assert type(Cloudrun(token)) is Cloudrun
     assert Cloudrun(token).token == token
 
-#def test_run_init():
-#    assert type(Run(token,id)) is Run
-#    assert Run(token,id).token == token
+@responses.activate
+def test_run_create():
+
+    data = json.load(open('cloudrun/test_data/run_create.json'))
+
+    responses.add(responses.POST,'https://api.cloudrun.co/v1/wrf',
+                  json=data,status=200)
+
+    run = Run(token)
+    run.create('wrf','3.9')
+    
+    assert run.status == 'created'
+    assert run.model == 'wrf'
+    assert run.version == '3.9'
+    assert run.time_created == datetime.datetime(2017,8,16,12,53,31)
+    assert run.input_files == []
+    assert run.output_files == []
+    
+def test_run_init():
+    assert type(Run(token)) is Run
+    assert Run(token).token == token
 #    assert Run(token,id).id == id
 
 #def test_cloudrun_get_run_returns_run():
