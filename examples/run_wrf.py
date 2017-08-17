e9a944154a15bfe51079450445782e8354b35fc2
Milan Curcic <caomaco@gmail.com>
2017-08-17 17:20:53 -0400

    first run example

--- examples/run_wrf.py
+++ examples/run_wrf.py
@@ -12,5 +12,8 @@ api = Cloudrun(token)
 # Create a new WRF run instance
 run = api.create_run(model='wrf',version='3.9')
 
-run.upload('namelist.input')
+for input_file in ['namelist.input','wrfinput_d01','wrfbdy_d01']
+    print(input_file)
+    run.upload(input_file)
 
+run.start(cores=1)
