84dd782c4cb8e8041f050f7fc9fa6e32242ffe5a
Milan Curcic <caomaco@gmail.com>
2017-08-18 23:16:29 -0400

    add run.setup() to the run sequence

--- examples/run_wrf.py
+++ examples/run_wrf.py
@@ -12,8 +12,10 @@ api = Cloudrun(token)
 # Create a new WRF run instance
 run = api.create_run(model='wrf',version='3.9')
 
-for input_file in ['namelist.input','wrfinput_d01','wrfbdy_d01']
+for input_file in ['namelist.input','wrfinput_d01','wrfbdy_d01']:
     print(input_file)
     run.upload(input_file)
+
+run.setup()
 
 run.start(cores=1)
