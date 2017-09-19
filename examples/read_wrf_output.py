b6db08510a05583fed481ac9967d71308f0aaf32
Milan Curcic <caomaco@gmail.com>
2017-09-18 23:44:31 -0400

    clean-up example

--- examples/read_wrf_output.py
+++ examples/read_wrf_output.py
@@ -5,20 +5,22 @@ from datetime import datetime
 import numpy as np
 import os
 
-id = 'ead622aa054040d39c8fd741d19993f0'
-
 # Secret token
 token = os.environ['CLOUDRUN_API_TOKEN']
 
 # Create API session instance
 api = Cloudrun(token)
 
-# Create a new WRF run instance
-run = api.get_run(id)
+# Access an existing WRF run
+run = api.get_run('ead622aa054040d39c8fd741d19993f0')
 
+# Read 10-m u wind on 2016-12-25 12:00 UTC
 field,time,u10 = run.read_output('u10',time1=datetime(2016,12,25,12))
+
+# Read 10-m v wind on 2016-12-25 12:00 UTC
 field,time,v10 = run.read_output('v10',time1=datetime(2016,12,25,12))
 
+# Calculate wind speed from components
 wspd = np.sqrt(u10**2+v10**2)
 
 print('Maximum wind speed on '
