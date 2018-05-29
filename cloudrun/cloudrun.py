dfcccb9656b6607ebbc5daa367afb5ebdf3a048a
Milan Curcic <caomaco@gmail.com>
2018-05-29 16:33:53 -0400

    formatting

--- cloudrun/cloudrun.py
+++ cloudrun/cloudrun.py
@@ -5,19 +5,19 @@ from .run import Run
 
 class Cloudrun(object):
     """Class to handle a Cloudrun API session."""
-    def __init__(self,token):
+    def __init__(self, token):
         """Cloudrun constructor."""
         self.token = token
 
-    def create_run(self,model,version):
+    def create_run(self, model, version):
         """Creates a new Run instance."""
         run = Run(self.token)
-        run.create(model,version)
+        run.create(model, version)
         return run
 
-    def get_run(self,id):
+    def get_run(self, id):
         """Returns an instance of model run by id."""
-        return Run(self.token,id)
+        return Run(self.token, id)
 
     def get_all_runs(self):
         """Returns a a list of all model runs that are 
