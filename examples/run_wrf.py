8375720c418558d3f7211c1db94b54bfa3f24f4d
Milan Curcic <caomaco@gmail.com>
2018-05-30 13:08:49 -0400

    clean up

--- examples/run_wrf.py
+++ examples/run_wrf.py
@@ -5,12 +5,11 @@ import os
 
 # Secret token
 token = os.environ['CLOUDRUN_API_TOKEN']
-
-userid = '8d3eee40-1700-11e7-9970-f584c576605b'
 url = 'https://api.cloudrun.co/v1'
+userid = '8d3eee40-1700-11e7-9970-f584c576605b'
 
 run = Run(url, token + '|' + userid)
-run.create(model='wrf', version='3.9.1')
+run.create('wrf', '3.9.1')
 
 # Upload input files
 for input_file in ['namelist.input', 'wrfinput_d01', 'wrfbdy_d01']:
