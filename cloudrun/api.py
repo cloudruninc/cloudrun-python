ddefe438b8c740df246dc6a6df7b8c3bd005949e
Josh NOe <josh.noe@gmail.com>
2018-06-18 13:02:09 -0700

    Added `start_forecast_run` method

--- cloudrun/api.py
+++ cloudrun/api.py
@@ -9,7 +9,7 @@ class Api:
     
     def __init__(self, url, token, debug=False, timer=False):
         """Returns a new Cloudrun session."""
-        self.url = self._cleanse_url(url)
+        self.base_url = self._cleanse_url(url)
         self.token = token
         self.debug = debug
         self.timer = timer
@@ -18,7 +18,7 @@ class Api:
 
     def create_run(self, model, version):
         """Creates a new run."""
-        url = self.url + '/runs'
+        url = self.base_url + '/runs'
         headers = {'Authorization': 'Bearer ' + self.token}
         data = {'model': model, 'version': version}
         r = requests.post(url, headers=headers, data=data)
@@ -26,28 +26,28 @@ class Api:
 
     def delete_run(self, runid):
         """Deletes an existing run."""
-        url = self.url + '/runs/' + runid
+        url = self.base_url + '/runs/' + runid
         headers = {'Authorization': 'Bearer ' + self.token}
         r = requests.delete(url, headers=headers)
         return r.status_code, r.json()
 
     def get_run(self, runid):
         """Returns an existing run."""
-        url = self.url + '/runs/' + runid
+        url = self.base_url + '/runs/' + runid
         headers = {'Authorization': 'Bearer ' + self.token}
         r = requests.get(url, headers=headers)
         return r.status_code, r.json()
 
     def patch_run(self, runid, json_patch):
         """Applies a JSON patch to a run."""
-        url = self.url + '/runs/' + runid
+        url = self.base_url + '/runs/' + runid
         headers = {'Authorization': 'Bearer ' + self.token}
         r = requests.patch(url, headers=headers, json=json_patch)
         return r.status_code, r.json()
 
     def start_run(self, runid):
         """Starts a run."""
-        url = self.url + '/runs/' + runid + '/start'
+        url = self.base_url + '/runs/' + runid + '/start'
         headers = {'Authorization': 'Bearer ' + self.token}
         r = requests.post(url, headers=headers)
         return r.status_code, r.json()
@@ -56,7 +56,7 @@ class Api:
         """Uploads a file to a run."""
         headers = {'Authorization': 'Bearer ' + self.token,
             'Origin': 'cloudrun.co'}
-        url = self.url + '/runs/' + runid + '/input'
+        url = self.base_url + '/runs/' + runid + '/input'
         files = {'file': open(filename, 'rb')}
         r = requests.post(url, headers=headers, files=files)
         return r.status_code, r.json()
@@ -64,9 +64,19 @@ class Api:
     def get_forecasts_due_to_run(self):
         return self._get('/runs/forecasts-due-to-run')
 
+    def start_forecast_run(self, forecast_id):
+        return self._post('/runs/run-forecast/' + forecast_id)
+
     def _get(self, path):
-        full_url = self.url + '/' + self._cleanse_path(path)
-        response = requests.get(full_url, headers=self.headers)
+        return self._send_request('get', path)
+
+    def _post(self, path):
+        return self._send_request('post', path)
+
+    def _send_request(self, verb, path):
+        full_url = self.base_url + '/' + self._cleanse_path(path)
+        request_method = getattr(requests, verb)
+        response = request_method(full_url, headers=self.headers)
         return response.status_code, response.json()
 
     def _cleanse_url(self, url):
